66185bafb6939fd84c513a97bada4987308ab88e
Denzel Mwaura <324699513+zDenzelMurphy@users.noreply.github.com>
2026-09-20 02:23:00 +0200

    Delete abweichungsanalyse_mögliche_ursache.sql

--- abweichungsanalyse_mögliche_ursache.sql
+++
@@ -1,5 +0,0 @@
-SELECT Buchungstext, COUNT(*) AS Anzahl, SUM(Betrag_Ist) AS Summe
-FROM buchungen
-WHERE KostenstellenID = 'K500' AND KontoID = 'A50' AND Datum LIKE '2026-10%'
-GROUP BY Buchungstext
-ORDER BY Summe DESC;
